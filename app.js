eca4336adbf384e053bee3e35f9719e4cf36f3a3
yonasketema <yonask.kitaw@gmail.com>
2024-02-18 15:06:38 +0300

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express");
+const cors = require("cors");
 const morgan = require("morgan");
 
-const app = express();
+const songRouter = require("./src/routes/songRoute");
 
+const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
+
+app.use("/api/v1/songs", songRouter);
 
 module.exports = app;
